45cc8f3844416dd567278371ef36636892265396
romalms10 <romalms10@gmail.com>
2018-08-08 22:51:57 +0000

    API endpoint /api/repos that returns all unique repo names

--- server/index.js
+++ server/index.js
@@ -138,6 +138,16 @@ app.prepare().then(() => {
     ctx.body = snapshot.map(doc => doc.data())
   });
 
+  // API endpoint to get all unique repo names
+  router.get("/api/repos", async ctx => {
+    const { req, res, user } = ctx;
+    const buildsRef = admin.firestore().collection('builds');
+    const snapshot = await buildsRef.get();
+    const repoArray = snapshot.docs.map(doc => doc._fieldsProto.repo.stringValue);
+    
+    ctx.body = [... new Set(repoArray)];
+  });
+
   // API endpoint to list an array of unique repo names
   // /api/repos
 
