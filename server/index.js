e6e12bba2b1d78baedc07d3d8866da60cf9da045
Billy Vong <github@mmo.me>
2018-08-09 10:13:20 -0700

    ref(): Move API auth to module

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const cookie = require("koa-cookie").default;
 const uuidv4 = require("uuid/v4");
 
 const admin = require("./admin");
+const apiAuth = require("./middleware/apiAuth");
 const githubHooksHandler = require("./hooks/github");
 
 const bucket = admin.storage().bucket();
@@ -35,56 +36,7 @@ app.prepare().then(() => {
   // The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
   // `Authorization: Bearer <Firebase ID Token>`.
   // when decoded successfully, the ID Token content will be added as `req.user`.
-  router.use(async (ctx, next) => {
-    const { req, request, cookie } = ctx;
-
-    if (!request.path.startsWith("/api/")) {
-      await next();
-      return;
-    }
-
-    // console.log("cookies", cookie);
-    if (
-      (!req.headers.authorization ||
-        !req.headers.authorization.startsWith("Bearer ")) &&
-      !(cookie && cookie.__session)
-    ) {
-      console.error(
-        "No Firebase ID token was passed as a Bearer token in the Authorization header.",
-        "Make sure you authorize your request by providing the following HTTP header:",
-        "Authorization: Bearer <Firebase ID Token>",
-        'or by passing a "session" cookie.'
-      );
-      ctx.status = 403;
-      return;
-    }
-
-    let idToken;
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer ")
-    ) {
-      // Read the ID Token from the Authorization header.
-      idToken = req.headers.authorization.split("Bearer ")[1];
-    } else if (cookie && cookie.__session) {
-      console.log('Found "session" cookie', cookie.__session);
-      // Read the ID Token from cookie.
-      idToken = cookie.__session;
-    } else {
-      // No cookie
-      ctx.status = 403;
-      return;
-    }
-
-    try {
-      const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-      ctx.user = decodedIdToken;
-      await next();
-    } catch (error) {
-      console.error("Error while verifying Firebase ID token", error);
-      ctx.status = 403;
-    }
-  });
+  router.use(apiAuth());
 
   // Create token for user
   router.post("/api/token", async ctx => {
